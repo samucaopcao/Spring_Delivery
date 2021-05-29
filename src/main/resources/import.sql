6701fb685a3d67db7634a3288a4a9a6f5f9dad83
Samuel Macedo <samuelmacedo2011@gmail.com>
2021-05-28 21:00:46 -0300

    Importando dados de teste com import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+Insert into cozinha(nome) values('Tailandesa');
+Insert into cozinha(nome) values('Indiana');
+Insert into cozinha(nome) values('Japonesa');
+Insert into cozinha(nome) values('Arabe');
+ 
