c18452053725c647e32aee6c215a812d04d05051
Samuel Macedo <samuelmacedo2011@gmail.com>
2021-06-10 15:30:40 -0300

    Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -6,9 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@JsonRootName("Teste_Cozinha")
 @Data // Anotação do Lombok que representa o @Getter @Setter e @EqualsAndHashCode
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -19,6 +22,9 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	// @JsonIgnore - Não mostra esse atributo na Resposta (no Postman)
+	// @JsonProperty("Título") - Altera a visualização do atributo nome para titulo
+	// ao ser mostrado na resposta
 	@Column(nullable = false)
 	private String nome;
 
